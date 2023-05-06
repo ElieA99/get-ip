0d4dbc8dd162f76b5b05ecf8fc6b37f75215a942
ElieA99 <elie_aouad1999@hotmail.com>
2023-05-06 19:29:40 +0300

    Ready

---
+++ Readme.md
@@ -0,0 +1,1 @@
+simple nodejs code to check your ip address using 'ip npm package'
---
+++ index.js
@@ -0,0 +1,5 @@
+ip = require('ip');
+colors = require('colors');
+
+ip = ip.address()
+console.log(`The IP is ${ip}`.blue)
